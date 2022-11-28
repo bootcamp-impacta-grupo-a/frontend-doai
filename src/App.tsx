6e49237945486f7fc1e6bfd1652346b99db901e0
dxwebster <adrianalimafm@gmail.com>
2022-11-28 18:11:55 -0300

    remove useRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, HashRouter, useNavigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css'
 import Login from './pages/Login'
 import Home from './pages/Home'
